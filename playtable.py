f310ef3d604c1a63cd0254a95707b90001fff603
Slavlen <slavlen2001@gmail.com>
2022-02-07 22:38:41 +0300

    add комментарии

--- playtable.py
+++ playtable.py
@@ -20,24 +20,24 @@ game_over = False
 def check_win(gametable, sign):
     zeroes = 0
 
-    for row in gametable:
+    for row in gametable: #проверка свободных ячеек
         zeroes += row.count(0)
 
-    for row in range(10):
+    for row in range(10): #горизонтальная проверерка
         for column in range(6):
             if gametable[row][column] == sign and gametable[row][column + 1] == sign and gametable[row][
                 column + 2] == sign and \
                     gametable[row][column + 3] == sign and gametable[row][column + 4] == sign:
                 return sign
 
-    for row in range(6):
+    for row in range(6): #вертикальная проверка
         for column in range(10):
             if gametable[row][column] == sign and gametable[row + 1][column] == sign and gametable[row + 2][
                 column] == sign and \
                     gametable[row + 3][column] == sign and gametable[row + 4][column] == sign:
                 return sign
 
-    for row in range(6):
+    for row in range(6): #горизонтальная проверка
         for column in range(6):
             if gametable[row][column] == sign and gametable[row + 1][column + 1] == sign and gametable[row + 2][
                 column + 2] == sign and \
@@ -60,10 +60,10 @@ def check_win(gametable, sign):
 def check_event():
     global query, game_over, gametable
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #закрыть игру
             pygame.quit()
             sys.exit(0)
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not game_over and query % 2 == 1:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not game_over and query % 2 == 1: #левый клик
             x_mouse, y_mouse = pygame.mouse.get_pos()
             column = x_mouse // (size_block + margin)
             row = y_mouse // (size_block + margin)
@@ -71,10 +71,10 @@ def check_event():
                 gametable[row][column] = 'x'
                 query += 1
 
-        elif query % 2 == 0 and not game_over:
+        elif query % 2 == 0 and not game_over: #ход компа
             ai()
 
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3: #правый клик
             game_over = False
             gametable = [[0] * 10 for i in range(10)]
             query = 1
@@ -123,7 +123,7 @@ def draw_table():
     pygame.display.update()
 
 
-def ai():
+def ai(): #бот с тактикой тыка
     global query, gametable, game_over
     if query % 2 == 0:
         random_x = random.randint(90, 900) // 100
